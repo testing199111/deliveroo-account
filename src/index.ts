2735357bd405b2a7d87c85562296f47b277c564a
testing199111 <dotesting1991@gmail.com>
2024-09-02 17:38:20 +0800

    feat: add slack integration

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+const slackUtils = require("./slack/index.ts");
+
 const generateRandomString = require("./helper");
 const smsActivateApi = require("./sms-activate/api");
 const smsActivate = require("./sms-activate/index.ts");
@@ -38,24 +40,26 @@ console.log("Account details:", accountDetails);
 const createAccount = async () => {
   console.log("Launching browser...");
   puppeteer
-    .launch({ headless: false, args: ["--no-sandbox", "--disable-setuid-sandbox"] })
+    .launch({
+      headless: false,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    })
     .then(async (browser: any) => {
       let page;
       try {
-        
         console.log("Browser launched.");
-  
-        
+
         page = await browser.newPage();
         await page.setViewport({ width: 1920, height: 1080 });
         console.log("New page opened.");
-  
+
         page.on("response", async (response: any) => {
           // console.log("Response URL:", response.url());
           try {
-            
             if (
-              response.url().includes("https://deliveroo.hk/send_verification_code")
+              response
+                .url()
+                .includes("https://deliveroo.hk/send_verification_code")
             ) {
               console.log("send verification code");
               const status = response.status();
@@ -66,10 +70,8 @@ const createAccount = async () => {
                 connectionStatus.sentVerificationCode = true;
               }
             }
-    
-            if (
-              response.url().includes("https://deliveroo.hk/verify_code")
-            ) {
+
+            if (response.url().includes("https://deliveroo.hk/verify_code")) {
               console.log("verify code response");
               const status = response.status();
               console.log("Response status:", status);
@@ -79,7 +81,7 @@ const createAccount = async () => {
                 connectionStatus.verifyCode = true;
               }
             }
-    
+
             if (
               response
                 .url()
@@ -94,11 +96,11 @@ const createAccount = async () => {
                 connectionStatus.createAccount = true;
               }
             }
-    
+
             if (response.url().includes("vouchers")) {
               const voucherUrlPattern =
                 /https:\/\/api\.hk\.deliveroo\.com\/orderapp\/v1\/users\/[^/]+\/vouchers/;
-              if (!voucherUrlPattern.test(response.url())) return
+              if (!voucherUrlPattern.test(response.url())) return;
               console.log("voucher response", response.url());
               const status = response.status();
               console.log("Response status:", status);
@@ -112,10 +114,10 @@ const createAccount = async () => {
             console.error("Error:", error);
           }
         });
-  
+
         const userAgent = new UserAgent().random().toString();
         await page.setUserAgent(userAgent);
-  
+
         console.log("Navigating to Deliveroo login page...");
         await page.goto("https://deliveroo.hk/en/login", {
           waitUntil: "networkidle2",
@@ -126,11 +128,11 @@ const createAccount = async () => {
         // console.log('Current page content:', content);
 
         await page.screenshot({
-          path: 'landing.png',
+          path: "landing.png",
         });
 
         try {
-          await page.waitForSelector('#continue-with-email', {
+          await page.waitForSelector("#continue-with-email", {
             timeout: 20000,
           });
           console.log("Next page loaded. (enter email)");
@@ -140,18 +142,17 @@ const createAccount = async () => {
 
         // Wait for the "Continue with email" button to be available
         await new Promise((r) => setTimeout(r, 5000));
-  
+
         console.log('Clicking "Continue with email" button...');
         await page.click("#continue-with-email");
         console.log('"Continue with email" button clicked.');
-  
+
         await page.type("#email-address", accountDetails.email);
         console.log("Random email entered into the input field.");
-  
+
         await page.click('button[type="submit"]');
         console.log('"Continue" button clicked.');
 
-        
         try {
           await page.waitForSelector('input[name="phone_number"]', {
             timeout: 10000,
@@ -160,22 +161,28 @@ const createAccount = async () => {
         } catch (error) {
           console.error("Next page did not load within the timeout period.");
         }
-        
+
         // throw Error("Test Error")
 
         const phoneNumberData = await smsActivateApi.requestPhoneNumber();
         accountDetails.phoneNumberId = phoneNumberData.id;
         accountDetails.phoneNumber =
           accountDetails.phoneNumber + phoneNumberData.phoneNumber;
-  
+
         const phoneInputField = await page.$('input[name="phone_number"]');
         // await phoneInputField.click({ clickCount: 4 });
         // await phoneInputField.type(String.fromCharCode(8)); // Clear the input field
-        await page.evaluate((input: any) => (input.value = ""), phoneInputField); // Clear the input field
-        await page.type('input[name="phone_number"]', accountDetails.phoneNumber);
-  
+        await page.evaluate(
+          (input: any) => (input.value = ""),
+          phoneInputField
+        ); // Clear the input field
+        await page.type(
+          'input[name="phone_number"]',
+          accountDetails.phoneNumber
+        );
+
         console.log("Phone number entered into the input field.");
-  
+
         await page.evaluate(() => {
           const buttons = Array.from(document.querySelectorAll("button"));
           const sendVerificationButton = buttons.find((button) =>
@@ -187,16 +194,16 @@ const createAccount = async () => {
           }
         });
         console.log('"Send verification code" button clicked.');
-  
+
         await new Promise((r) => setTimeout(r, 5000));
-  
+
         if (connectionStatus.sentVerificationCode) {
           console.log("Verification code sent successfully.");
           // Proceed with the next steps
         } else {
           throw Error("Failed to send verification code.");
         }
-  
+
         const smsCode = await smsActivate.checkPhoneNumberStatus(
           accountDetails.phoneNumberId
         );
@@ -205,12 +212,12 @@ const createAccount = async () => {
         await page.waitForSelector('input[name="phone_code"]', {
           timeout: 10000,
         });
-  
+
         // Type the SMS code into the text field
         await page.type('input[name="phone_code"]', smsCode);
         // await page.type('input[name="phone_code"]', "941220");
         console.log("SMS code entered into the text field.");
-  
+
         await page.evaluate(() => {
           const buttons = Array.from(document.querySelectorAll("button"));
           console.log("Buttons:", buttons);
@@ -225,26 +232,25 @@ const createAccount = async () => {
           verificationButton.click();
         });
         console.log('"Verify" button clicked.');
-  
+
         await new Promise((r) => setTimeout(r, 8000));
-  
+
         if (connectionStatus.verifyCode) {
           console.log("sms code verification successfully.");
           // Proceed with the next steps
         } else {
           throw Error("Failed to verify sms code.");
         }
-  
+
         await page.type("#fullName", accountDetails.firstName);
         console.log("fullName entered into the input field.");
-  
+
         await page.type("#preferredName", accountDetails.lastName);
         console.log("preferredName entered into the input field.");
-  
+
         await page.type("#password", accountDetails.password);
         console.log("password entered into the input field.");
-  
-  
+
         await page.evaluate(() => {
           const buttons = Array.from(document.querySelectorAll("button"));
           const CreateAccountButton = buttons.find((button) =>
@@ -256,30 +262,32 @@ const createAccount = async () => {
           console.log('"Create account" button is enabled.');
           CreateAccountButton.click();
         });
-  
+
         console.log('"Create account" button clicked.');
-  
+
         await new Promise((r) => setTimeout(r, 8000));
-  
+
         if (connectionStatus.createAccount) {
-          console.log("Created Account")
+          console.log("Created Account");
         } else {
           throw Error("Failed to create account.");
         }
-  
+
         console.log("Account Details:", accountDetails);
-  
+
         // ----------------------------------------------
-  
+
         console.log("Navigating to Deliveroo redeem coupon page...");
         await page.goto("https://deliveroo.hk/en/account/vouchers", {
           waitUntil: "networkidle2",
         });
         console.log("Navigation complete.");
-  
+
         await page.type("#voucher-code", accountDetails.couponCode);
-        console.log(`coupon code ${accountDetails.couponCode} entered into the input field.`);
-  
+        console.log(
+          `coupon code ${accountDetails.couponCode} entered into the input field.`
+        );
+
         await page.evaluate(() => {
           const buttons = Array.from(document.querySelectorAll("button"));
           const SubmitButton = buttons.find((button) =>
@@ -291,23 +299,27 @@ const createAccount = async () => {
           console.log('"Submit" button is enabled.');
           SubmitButton.click();
         });
-  
+
         console.log('"Submit account" button clicked.');
-  
+
         await new Promise((r) => setTimeout(r, 5000));
-  
+
         if (connectionStatus.redeemCoupon) {
-          console.log("Redeem Coupon Successfully")
+          console.log("Redeem Coupon Successfully");
+          if (process.env.SLACK_SEND === "true") {
+            await slackUtils.sendfullAccountDetails(accountDetails);
+            console.log("Account detail sent to slack");
+          }
         } else {
           throw Error("Failed to redeem coupon.");
         }
       } catch (error) {
-        throw Error(error as any)
+        throw Error(error as any);
       } finally {
         await page.screenshot({
-          path: 'final.png',
+          path: "final.png",
         });
-        console.log("screenshot captured")
+        console.log("screenshot captured");
         await browser.close();
         console.log("Browser closed.");
       }
